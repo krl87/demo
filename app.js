24d5f30017ae6ef5ecb25640ed735f1f91ddce17
krl87 <kayley.luftig@gmail.com>
2016-04-11 12:43:25 -0400

    Working Tutorial

--- app.js
+++ app.js
@@ -24,15 +24,15 @@ var app = express();
 
 // 7) set up local strategy?
 
-passport.use(new LocalStrategy(function(username, password, done) {
-    User.findOne({ username: username }, function(err, user) {
+passport.use(new LocalStrategy(function (username, password, done) {
+    User.findOne({username: username}, function (err, user) {
         if (err) return done(err);
-        if (!user) return done(null, false, { message: 'Incorrect username.' });
-        user.comparePassword(password, function(err, isMatch) {
+        if (!user) return done(null, false, {message: 'Incorrect username.'});
+        user.comparePassword(password, function (err, isMatch) {
             if (isMatch) {
                 return done(null, user);
             } else {
-                return done(null, false, { message: 'Incorrect password.' });
+                return done(null, false, {message: 'Incorrect password.'});
             }
         });
     });
@@ -40,12 +40,12 @@ passport.use(new LocalStrategy(function(username, password, done) {
 
 //8) serialize and deserialize
 
-passport.serializeUser(function(user, done){
+passport.serializeUser(function (user, done) {
     done(null, user.id);
 });
 
-passport.deserializeUser(function(id, done){
-    User.findById(id, function(err, user){
+passport.deserializeUser(function (id, done) {
+    User.findById(id, function (err, user) {
         done(err, user);
     });
 });
@@ -94,7 +94,7 @@ userSchema.pre('save', function (next) {
 // 5) password verification
 
 userSchema.methods.comparePassword = function (candidatePassword, cb) {
-    bcrypt.compare(candidatePassword, this.password, function(err, isMatch){
+    bcrypt.compare(candidatePassword, this.password, function (err, isMatch) {
         if (err) return cb(er);
         cb(null, isMatch);
     });
@@ -140,28 +140,28 @@ app.get('/', function (req, res, next) {
 
 
 //add login (keep)
-app.get('/login', function(req, res, next){
-    res.render('login',{
+app.get('/login', function (req, res, next) {
+    res.render('login', {
         title: 'Login',
         user: req.user
     });
 });
 
-app.post('/login', function(req, res, next){
-   passport.authenticate('local', function(err, user, info) {
-      if (err) return next(err)
-       if (!user) {
-           return res.redirect('/login')
-       }
-       req.logIn(user, function(err){
-          if (err) return next(err);
-           return res.redirect('/');
-       });
-   })(req, res, next);
+app.post('/login', function (req, res, next) {
+    passport.authenticate('local', function (err, user, info) {
+        if (err) return next(err)
+        if (!user) {
+            return res.redirect('/login')
+        }
+        req.logIn(user, function (err) {
+            if (err) return next(err);
+            return res.redirect('/');
+        });
+    })(req, res, next);
 });
 
 //sign up
-app.get('/signup', function(req, res, next){
+app.get('/signup', function (req, res, next) {
     res.render('signup', {
         title: 'Register',
         user: req.user
@@ -170,28 +170,28 @@ app.get('/signup', function(req, res, next){
 
 //create a POST route to handle the form on the signup page.
 
-app.post('/signup', function(req, res, next) {
-   var user = new User({
-      username: req.body.username,
-       email: req.body.email,
-       password: req.body.password
-   });
-    user.save(function(err) {
-       req.logIn(user, function(err) {
-          res.redirect('/');
-       });
+app.post('/signup', function (req, res, next) {
+    var user = new User({
+        username: req.body.username,
+        email: req.body.email,
+        password: req.body.password
+    });
+    user.save(function (err) {
+        req.logIn(user, function (err) {
+            res.redirect('/');
+        });
     });
 });
 
 //logout route
 
-app.get('/logout', function(req, res, next){
+app.get('/logout', function (req, res, next) {
     req.logout();
     res.redirect('/');
 });
 
 //forgot route handlers
-app.get('/forgot', function(req, res, next){
+app.get('/forgot', function (req, res, next) {
     res.render('forgot', {
         title: 'Reset Password',
         user: req.user
@@ -199,55 +199,119 @@ app.get('/forgot', function(req, res, next){
 });
 
 app.post('/forgot', function (req, res, next) {
-   async.waterfall([
-       function(done) {
-           crypto.randomBytes(20, function(err, buf){
-              var token = buf.toString('hex');
-               done(err, token);
-           });
-       },
-       function(token, done) {
-           User.findOne({ email: req.body.email}, function(err, user){
-              if (!user) {
-                  req.flash('error', 'No account with this email address exists');
-                  return res.redirect('/forgot')
-              }
-               user.resetPasswordToken = token;
-               user.resetPasswordExpires = Date.now() + 3600000;
+    async.waterfall([
+        function (done) {
+            crypto.randomBytes(20, function (err, buf) {
+                var token = buf.toString('hex');
+                done(err, token);
+            });
+        },
+        function (token, done) {
+            User.findOne({email: req.body.email}, function (err, user) {
+                if (!user) {
+                    req.flash('error', 'No account with this email address exists');
+                    return res.redirect('/forgot')
+                }
+                user.resetPasswordToken = token;
+                user.resetPasswordExpires = Date.now() + 3600000;
 
-               user.save(function(err){
-                  done(err, token, user);
-               });
-           });
-       },
-       function(token, user, done) {
-           var options = {
-               service: 'Mailgun',
-               auth: {
-                   user: 'postmaster@Sandbox65b418bcf76c4a5e909aedb7b6e87b45.mailgun.org',
-                   pass: '847f704c64e240d0f6cc29966c6f03ab'
-               }
-           };
-           var transporter = nodemailer.createTransport(smtpTransport(options))
+                user.save(function (err) {
+                    done(err, token, user);
+                });
+            });
+        },
+        function (token, user, done) {
+            var options = {
+                service: 'Mailgun',
+                auth: {
+                    user: 'postmaster@Sandbox65b418bcf76c4a5e909aedb7b6e87b45.mailgun.org',
+                    pass: '847f704c64e240d0f6cc29966c6f03ab'
+                }
+            };
+            var transporter = nodemailer.createTransport(smtpTransport(options))
 
-           var mailOptions = {
-               to: user.email,
-               from: 'postmaster@Sandbox65b418bcf76c4a5e909aedb7b6e87b45.mailgun.org',
-               subject: 'Node.js Password Reset',
-               text: 'You are receiving this because you (or someone else) have requested the reset of the password for your account.\n\n' +
-               'Please click on the following link, or paste this into your browser to complete the process:\n\n' +
-               'http://' + req.headers.host + '/reset/' + token + '\n\n' +
-               'If you did not request this, please ignore this email and your password will remain unchanged.\n'
-           };
-           transporter.sendMail(mailOptions, function(err){
-               req.flash('info', 'An e-mail has been sent to ' + user.email + ' with further instructions.');
-               done(err, 'done');
-           });
-       }
-   ], function(err){
-       if (err) return next(err);
-       res.redirect('/forgot');
-   });
+            var mailOptions = {
+                to: user.email,
+                from: 'postmaster@Sandbox65b418bcf76c4a5e909aedb7b6e87b45.mailgun.org',
+                subject: 'Node.js Password Reset',
+                text: 'You are receiving this because you (or someone else) have requested the reset of the password for your account.\n\n' +
+                'Please click on the following link, or paste this into your browser to complete the process:\n\n' +
+                'http://' + req.headers.host + '/reset/' + token + '\n\n' +
+                'If you did not request this, please ignore this email and your password will remain unchanged.\n'
+            };
+            transporter.sendMail(mailOptions, function (err) {
+                req.flash('info', 'An e-mail has been sent to ' + user.email + ' with further instructions.');
+                done(err, 'done');
+            });
+        }
+    ], function (err) {
+        if (err) return next(err);
+        res.redirect('/forgot');
+    });
+});
+
+
+//reset route
+app.get('/reset/:token', function (req, res, next) {
+    User.findOne({resetPasswordToken: req.params.token, resetPasswordExpires: {$gt: Date.now()}}, function (err, user) {
+        if (!user) {
+            req.flash('error', 'Password reset token is invalid or has expired.');
+            return res.redirect('/forgot');
+        }
+        res.render('reset', {
+            title: 'Reset Password',
+            user: req.user
+        });
+    });
+});
+
+
+//get handler for reset token
+app.post('/reset/:token', function(req, res, next) {
+    async.waterfall([
+        function(done) {
+            User.findOne({ resetPasswordToken: req.params.token, resetPasswordExpires: { $gt: Date.now() } }, function(err, user) {
+                if (!user) {
+                    req.flash('error', 'Password reset token is invalid or has expired.');
+                    return res.redirect('back');
+                }
+
+                user.password = req.body.password;
+                user.resetPasswordToken = undefined;
+                user.resetPasswordExpires = undefined;
+
+                user.save(function(err) {
+                    req.logIn(user, function(err) {
+                        done(err, user);
+                    });
+                });
+            });
+        },
+        function (token, user, done) {
+            var options = {
+                service: 'Mailgun',
+                auth: {
+                    user: 'postmaster@Sandbox65b418bcf76c4a5e909aedb7b6e87b45.mailgun.org',
+                    pass: '847f704c64e240d0f6cc29966c6f03ab'
+                }
+            };
+            var transporter = nodemailer.createTransport(smtpTransport(options))
+
+            var mailOptions = {
+                to: user.email,
+                from: 'postmaster@Sandbox65b418bcf76c4a5e909aedb7b6e87b45.mailgun.org',
+                subject: 'Your password has been changed',
+                text: 'Hello,\n\n' +
+                'This is a confirmation that the password for your account ' + user.email + ' has just been changed.\n'
+            };
+            smtpTransport.sendMail(mailOptions, function(err) {
+                req.flash('success', 'Success! Your password has been changed.');
+                done(err);
+            });
+        }
+    ], function(err) {
+        res.redirect('/');
+    });
 });
 
 
